6e6ca0db97c3a768070991881d9f2e2b44dc174c
Abolfazl <abolfazlhamsayeh69@gmail.com>
2023-10-30 14:11:01 +0330

    relations between layers

---
+++ Src/Web/Common/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Common
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BaseApiController : ControllerBase
+    {
+       
+    }
+}
